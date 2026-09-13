2fe8a4f80fb5b8432d04afeb6febd9b8bab617e9
OA <1834231+pullman-A0@users.noreply.github.com>
2026-09-13 17:40:12 +0300

    Add Yandex Metrika counter 112548626

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,28 @@ export default class MyDocument extends Document {
       <Html lang="ru">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(112548626, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`,
+            }}
+          />
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/112548626" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
         </Head>
         <body className="next-light-theme">
           <Main />
